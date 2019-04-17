205782313145971737a3e60f48835b12aa98ac6c
Jim <jim_king_2000@163.com>
2019-04-17 22:20:28 +0800

    refactory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { Grommet, Button } from 'grommet';
 import { dxc } from "grommet-theme-dxc";
 
+const C = () => <Button label='test' />;
+
 export const B = () => (
   <Grommet theme={dxc}>
-    <Button label='test' />
+    <C />
   </Grommet>
 );
